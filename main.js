bede021a4afb6c2119493908efb0cee54a3451e8
Scott Manley <scottmanley@SJM-MacBook-Pro.local>
2020-02-04 18:17:39 -0600

    fixed code to working

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 //symbols,numbers,upperCase,lowerCase//
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var symbols = ['!', '$', '%', '^', '&', '*', '(', '?', ':', ']', '[', ';']
 
+// var passwordLength = ("passwordLength");
 
 function passwordRequirements() {
     var passwordLength = parseInt(prompt("How long do you want your password?"));
@@ -21,21 +22,22 @@ function passwordRequirements() {
         alert("Please ensure your password is less than 128 characters");
         return;
     }
-    var containsSymbols = confirm("Click OK button to confirm special characters");
+    var containSymbols = confirm("Click OK button to confirm special characters");
     var containUppercase = confirm("Click OK button to confirm Upper Case letters");
     var containLowercase = confirm("Click OK button to confirm Lower Case letters");
     var containNumbers = confirm("Click OK button to confirm Numbers");
 
-    if (containsSymbols === false && containUppercase === false && containLowercase === false && containNumbers === false )    
-    { alert ("Please go back and pick one number or symbol");
+    if (containSymbols === false && containUppercase === false && containLowercase === false && containNumbers === false) {
+        alert("Please go back and pick one number or symbol");
         return;
     }
+    console.log(containSymbols, containUppercase, containLowercase, containNumbers)
 
-
+    //storages characters
     var passwordTypes = {
-        passwordLength: passwordLength, 
-        containsSymbols: containsSymbols,
-        containLowerCase: containLowercase,
+        passwordLength: passwordLength,
+        containSymbols: containSymbols,
+        containLowercase: containLowercase,
         containNumbers: containNumbers,
         containUppercase: containUppercase
     };
@@ -43,7 +45,7 @@ function passwordRequirements() {
 
     return passwordTypes;
 
-}   
+}
 function randomize(arr) {
     var index = Math.floor(Math.random() * arr.length);
     var element = arr[index];
@@ -53,62 +55,69 @@ function randomize(arr) {
 
 function passwordGenerator() {
     var options = passwordRequirements();
+    console.log(options)
+
     var passwordResult = [];
     var potentialCharacters = [];
     var guaranteedCharacters = [];
 
-if (options.containsSymbols){
-    potentialCharacters = potentialCharacters.concat(symbols);
-    guaranteedCharacters.push(randomize(symbols));
-}
-if (options.containUppercase){
-    potentialCharacters = potentialCharacters.concat(upperCase);
-    guaranteedCharacters.push(randomize(upperCase));
-}
-if (options.containLowercase){
-    potentialCharacters = potentialCharacters.concat(lowerCase);
-    guaranteedCharacters.push(randomize(lowerCase));
-}
-if (options.containNumbers){
-    potentialCharacters = potentialCharacters.concat(numbers);
-    guaranteedCharacters.push(randomize(numbers));
-}
-    for (var i = 0; i < passwordLength.length; i++){
+    if (options.containSymbols) {
+        potentialCharacters = potentialCharacters.concat(symbols);
+        guaranteedCharacters.push(randomize(symbols));
+        console.log("guaranteedCharacters", guaranteedCharacters);
+    }
+    if (options.containUppercase) {
+        potentialCharacters = potentialCharacters.concat(uppercase);
+        guaranteedCharacters.push(randomize(uppercase));
+        console.log("guaranteedCharacters", guaranteedCharacters);
+    }
+    if (options.containLowercase) {
+        potentialCharacters = potentialCharacters.concat(lowercase);
+        guaranteedCharacters.push(randomize(lowercase));
+        console.log("guaranteedCharacters", guaranteedCharacters);
+    }
+    if (options.containNumbers) {
+        potentialCharacters = potentialCharacters.concat(numbers);
+        guaranteedCharacters.push(randomize(numbers));
+        console.log("guaranteedCharacters", guaranteedCharacters);
+    }
+    for (var i = 0; i < options.passwordLength; i++) {
         var potentialCharacter = randomize(potentialCharacters);
         passwordResult.push(potentialCharacter);
     }
-    for (var i = 0; i < guaranteedCharacters.length; i++){
+    for (var i = 0; i < guaranteedCharacters.length; i++) {
         passwordResult[i] = guaranteedCharacters[i];
-    } 
-   
+    }
+
     return passwordResult.join("");
-}  
+}
 
-var myPassword = passwordGenerator();
-console.log(myPassword)
-var copy = document.getElementById('generate1');
-var clipboard = document.getElementById('generate2');
+var copyBtn = document.querySelector("#copy");
+var generateBtn = document.querySelector("#generate1");
 
-function printPassword (){
+
+function printPassword() {
     var pw = passwordGenerator();
     var pwPrint = document.getElementById('password');
     pwPrint.value = pw;
 }
 
-console.log (printPassword);
+console.log(printPassword);
 
-// function copyToClipboard() {
-//     document.getElementById("password").select();
 
-//     document.execCommand("Copy");
+function copyToClipboard() {
+    var passwordText = document.querySelector("#password");
 
-//     // let password = []
-//     // while (password.length < length) {
-//     //     const rnd = Math.floor(Math.random() * avail.length);
-//     //     password.push(avail[rnd]);
-//     // }
-// console.log (copyToClipboard);
+    passwordText.select();
+    document.execCommand("copy");
 
-//     alert("Password copied to clipboard");
-// }
+    alert(
+        "Your password " + passwordText.value + " was copied to your clipboard."
+    );
+}
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", printPassword);
+
+// Add event listener to copy button
+copyBtn.addEventListener("click", copyToClipboard);
